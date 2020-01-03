27713f3d365b7968684525fd722ca6f0c392a90f
Abedron <abedron@outlook.com>
2020-01-03 15:31:55 +0100

    add config

---
+++ src/Config.h
@@ -0,0 +1,7 @@
+const char *SSID = "...";
+const char *PSK = "...";
+const char *ApiKey = "...";
+unsigned long ChannelId = 0;
+
+// 15e6 is 15 seconds, 900e6 is 15 minutes
+uint64_t DeepSleepTime = 900e6;
